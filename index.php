a8f831e8945c98909065a07cf1253c00c935304c
German <german.tammekunn@gmail.com>
2026-09-08 08:44:51 +0300

    2. tund

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$test = 10;
+$test = 3.1;
+$test = "Hello";
+$test = "opilane";
+$test = true;
+$test = false;
+$test = NULL;
+$test = [1, null, true, "lol", 2, 3.2];
+$test = new stdClass();
+$test->name = "Gerru";
+
+
+
+var_dump($test);
+
+
+?>
+
+
+
+
+
